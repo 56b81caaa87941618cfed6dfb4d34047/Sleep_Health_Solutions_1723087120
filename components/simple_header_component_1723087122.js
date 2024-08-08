469587d7438e41a44927db002dadb168578d63e9
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-08 03:32:03 +0000

    Code generation

--- components/simple_header_component_1723087122.js
+++ components/simple_header_component_1723087122.js
@@ -29,6 +29,7 @@ Vue.component("simple_header_component_1723087122", {
                         <li id="nav-item-3"><a href="#" class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Follow-up Consultations</a></li>
                         <li id="nav-item-4"><a href="#" class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-purple-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">About</a></li>
                     </ul>
+                    </ul>
                 </div>
             </div>
         </nav>
